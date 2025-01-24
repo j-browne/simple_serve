73cd5ea65d1de0322f34d11aa550a08fc54495dc
Justin Browne <jebdude89@gmail.com>
2025-01-24 01:40:09 -0500

    Support binding to multiple addresses

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use axum::Router;
 use clap::Parser;
 use std::{
     error::Error,
-    net::{IpAddr, SocketAddr},
+    future::IntoFuture,
+    net::{IpAddr, Ipv6Addr, SocketAddr},
     path::PathBuf,
 };
+use tokio::task::JoinSet;
 use tower_http::services::ServeDir;
 
 /// Serve a directory.
@@ -13,21 +15,41 @@ struct Args {
     /// Directory to serve.
     #[arg(default_value = ".")]
     path: PathBuf,
-    /// IP address to bind to.
-    #[arg(short, long, default_value = "::")]
-    address: IpAddr,
-    /// Port to bind to. If 0, the OS will assign a port.
-    #[arg(short, long, default_value_t = 0)]
-    port: u16,
+    /// Socket address to bind to.
+    ///
+    /// This option may be specified multiple times.
+    ///
+    /// If the port is 0, the OS will assign a port.
+    ///
+    /// If this option is not specified, it will default to
+    /// listening on all ipv6 addresses, with an OS-supplied port.
+    /// Depending on the OS, this may listen on all ipv4 addresses, as well.
+    /// [default: [::]:0]
+    #[arg(short, long = "address", id = "ADDR")]
+    addrs: Vec<SocketAddr>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let args = Args::parse();
+    let mut args = Args::parse();
     let app = Router::new().fallback_service(ServeDir::new(args.path.clone()));
-    let addr = SocketAddr::new(args.address, args.port);
-    let listener = tokio::net::TcpListener::bind(addr).await?;
-    println!("Serving {:?} on {:?}", args.path, listener.local_addr()?);
-    axum::serve(listener, app).await?;
+
+    // Set default addr
+    if args.addrs.is_empty() {
+        args.addrs
+            .push(SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), 0));
+    }
+
+    // Spawn all the tasks. If any finishes, return the error
+    let mut join_set = JoinSet::new();
+    for addr in args.addrs {
+        let listener = tokio::net::TcpListener::bind(addr).await?;
+        println!("Serving {:?} on {:?}", args.path, listener.local_addr()?);
+        join_set.spawn(axum::serve(listener, app.clone()).into_future());
+    }
+    while let Some(res) = join_set.join_next().await {
+        let _ = res?;
+    }
+
     Ok(())
 }
